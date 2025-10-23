17d35b2d76e06d0e2a9aee4e6ea144d147561a7a
Basil132004 <basilkafayat@gmail.com>
2025-10-23 19:05:44 +0500

    Add files via upload

--- task1.c
+++ task1.c
@@ -1,45 +1,28 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<sys/wait.h>
 #include<unistd.h>
-#include<string.h>
-int Read =0;
-int wright=1;
-int main(int argumnent, char*argArr[]){
-int size =argumnent-1;
-int *arr = (int*)malloc(size*sizeof (int));
-int *Rarr = (int*)malloc(size*sizeof (int));
-for(int i =0;i<size;i++)
-arr[i]=atoi(argArr[i+1]);
 
-int pip[2];
-int bytesread;
-pipe(pip);
-if(fork()==0){
-
-    close(pip[Read]);
-    for(int i=0;i<size-1;i++)
-    for(int j=0;j<size-1;j++){
-        if(arr[j]>arr[j+1])
-        {
-            int temp=arr[j];
-            arr[j]=arr[j+1];
-            arr[j+1]=temp;
-        }
-
-    }
-    write(pip[wright],arr,size*sizeof(int));
-    close(pip[wright]);
-}
-else /* parent, reader */
+int main(int argc, char *argv[])
 {
-close ( pip [wright] ) ; /* close unused end */
-bytesread = read (pip [Read], Rarr, size*sizeof(int)) ;
-int d;
-for(int i=0;i<size;i++){
-d=Rarr[i];
-printf ("Read %d bytes :%d\n", bytesread, d) ;}
-close ( pip [Read] ) ; /* close used end */
-}
+int id=fork();
+if(id==0)
+{
+    printf("child process started PID= %d, parent pid %d \n",getpid(),getppid());
+printf("child running....\n");
+        sleep(3);
+        printf("child finished.\n");
 
+}
+else {
+
+ printf("pareent process PID = %d \n",getpid());
+        
+            if(wait(NULL)==-1)
+           printf("error still waiting for child..\n");
+        else
+        printf("parent detected child termination.\n");
+          
+    
+}
     return 0;
 }
